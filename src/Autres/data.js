db4fb238dd31f2f5c5e0d9599f2c684e0b71dc17
Kenne Tsasse Francis <harfleur20@gmail.com>
2025-10-16 08:04:48 +0200

    update-f1

---
+++ src/Autres/data.js
@@ -0,0 +1,19 @@
+import home1 from '../assets/home1.png'
+import home2 from '../assets/home2.png'
+import home3 from '../assets/home3.png'
+import home4 from '../assets/home4.png'
+import home5 from '../assets/home5.png'
+import home6 from '../assets/home6.png'
+import home7 from '../assets/home7.png'
+import home8 from '../assets/home8.png' 
+
+export const images = {
+    home1,
+    home2,
+    home3,
+    home4,
+    home5,
+    home6,
+    home7,
+    home8
+}
